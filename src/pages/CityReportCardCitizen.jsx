0885d03d3fb0b18c3ef8ae67a731b32b4dc78c94
IshitaSri8 <ishita.s@arahas.com>
2024-10-18 18:04:05 +0530

    ui modifications

--- src/pages/CityReportCardCitizen.jsx
+++ src/pages/CityReportCardCitizen.jsx
@@ -8,7 +8,7 @@ const CityReportCardCitizen = ({ show }) => {
   const [activeTab, setActiveTab] = useState(0);
 
   return (
-    <div className="px-2 py-4">
+    <div className="px-2 py-4 sec-theme">
       <h1 className="text-primary1 text-xl text-left text-medium w-full m-0 p-0 ml-3 mb-3">
         City Report Card
         {show}
