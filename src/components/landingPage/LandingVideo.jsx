bba9f925ae4f52135ed94960a36312592aae53a9
IshitaSri8 <ishita.s@arahas.com>
2025-01-29 14:31:43 +0530

    landing animation added

--- src/components/landingPage/LandingVideo.jsx
+++ src/components/landingPage/LandingVideo.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import landing from "assets/landing3.mp4";
 import { useRef } from "react";
 import { useEffect } from "react";
 
@@ -27,7 +26,7 @@ const LandingVideo = () => {
       >
         <source
           // src="https://res.cloudinary.com/dqbjijwmy/video/upload/v1737617468/CSI-videos/mb2okycoedpl8lca40zs.mp4"
-          src={landing}
+          src="https://res.cloudinary.com/dqbjijwmy/video/upload/v1738141276/CSI-videos/db7qssegrcy6yxuybsas.mp4"
           type="video/mp4"
         />
       </video>
