7274ae4ebe9b93c9372d84ed4948478a8ffbe494
Yashi Yadav <yashi.y@arahastechnologies.com>
2025-01-31 10:28:41 +0530

    changes

--- src/components/landingPage/LandingVideo.jsx
+++ src/components/landingPage/LandingVideo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useRef } from "react";
 import { useEffect } from "react";
+import landing from "assets/landing_page.mp4";
 
 const LandingVideo = () => {
   const videoRef = useRef(null); // Create a ref for the video element
@@ -28,7 +29,8 @@ const LandingVideo = () => {
       <source
         // src="https://res.cloudinary.com/dqbjijwmy/video/upload/v1737617468/CSI-videos/mb2okycoedpl8lca40zs.mp4"
         // src="https://res.cloudinary.com/dqbjijwmy/video/upload/v1738141276/CSI-videos/db7qssegrcy6yxuybsas.mp4"
-        src="https://res.cloudinary.com/dqbjijwmy/video/upload/v1738143177/CSI-videos/bwd4mh2hbstjv8hkgud8.mp4"
+        // src="https://res.cloudinary.com/dqbjijwmy/video/upload/v1738143177/CSI-videos/bwd4mh2hbstjv8hkgud8.mp4"
+        src={landing}
         type="video/mp4"
       />
     </video>
