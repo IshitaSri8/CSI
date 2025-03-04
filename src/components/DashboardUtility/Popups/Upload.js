aaf94f60d4718caca59cf7d75b578a869af5600e
IshitaSri8 <ishita.s@arahas.com>
2025-03-04 16:17:39 +0530

    added healthcare template

--- src/components/DashboardUtility/Popups/Upload.js
+++ src/components/DashboardUtility/Popups/Upload.js
@@ -70,6 +70,9 @@ const Upload = ({ visible, onHide, parameter }) => {
     if (parameter === "transport") {
       url = `${process.env.PUBLIC_URL}/Transport.xlsx`;
     }
+    if (parameter === "healthcare") {
+      url = `${process.env.PUBLIC_URL}/Healthcare.xlsx`;
+    }
 
     const link = document.createElement("a");
     link.href = url;
